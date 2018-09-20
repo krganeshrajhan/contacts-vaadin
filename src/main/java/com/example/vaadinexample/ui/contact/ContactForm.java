b5588162560132256c65f435e41a66aa24ad12d7
krganeshrajhan <krganeshrajhan@gmail.com>
2018-09-20 20:49:53 +0800

    Contact UI

---
+++ src/main/java/com/example/vaadinexample/ui/contact/ContactForm.java
@@ -0,0 +1,105 @@
+package com.example.vaadinexample.ui.contact;
+
+import com.example.vaadinexample.model.Contact;
+import com.example.vaadinexample.service.ContactService;
+import com.vaadin.data.Binder;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static com.vaadin.ui.themes.ValoTheme.BUTTON_DANGER;
+import static com.vaadin.ui.themes.ValoTheme.BUTTON_PRIMARY;
+
+public class ContactForm extends FormLayout {
+
+    private TextField firstName = new TextField("First Name");
+    private TextField lastName = new TextField("Last Name");
+    private Button save = new Button("Save");
+    private Button cancel = new Button("Cancel");
+    private Button delete = new Button("Delete");
+    private ContactUI contactUI;
+    private Binder<Contact> binder = new Binder<>(Contact.class);
+    private Contact contact;
+    private final VerticalLayout popupWindow = new VerticalLayout();
+    private PopupView confirm = new PopupView("Confirm", popupWindow);
+
+    @Autowired
+    private ContactService contactService;
+
+    public ContactForm(ContactUI contactUI) {
+        this.contactUI = contactUI;
+        HorizontalLayout buttons = new HorizontalLayout(save, cancel, delete);
+        addComponents(firstName, lastName, buttons);
+        binder.bindInstanceFields(this);
+
+        save.setStyleName(BUTTON_PRIMARY);
+        save.setIcon(VaadinIcons.PLUS_CIRCLE);
+        save.addClickListener(e -> save());
+
+        cancel.addClickListener(e -> cancel());
+
+        delete.setStyleName(BUTTON_DANGER);
+        delete.setIcon(VaadinIcons.MINUS_CIRCLE);
+        delete.addClickListener(e -> delete());
+
+        binder.addValueChangeListener(e -> {
+            cancel.setVisible(true);
+        });
+        confirm.setPopupVisible(false);
+        confirm.addPopupVisibilityListener(e -> {
+        });
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+        binder.setBean(contact);
+        delete.setVisible(contact.getId() != null);
+        cancel.setVisible(false);
+        setVisible(true);
+    }
+
+    public void save() {
+        System.out.println(contactService + " " + contact);
+        contactUI.getContactService().save(contact);
+        contactUI.updateContacts();
+        setVisible(false);
+        Notification.show("Contact Saved");
+    }
+
+    public void cancel() {
+
+        if (contact.getId() != null) {
+            List<Contact> contacts = contactUI.getContactService().findByIdOrderById(contact.getId());
+            binder.setBean(contacts.get(0));
+        } else {
+            binder.setBean(new Contact());
+        }
+        cancel.setVisible(false);
+
+    }
+
+    public void delete() {
+
+        final Window sub = new Window("Confirm");
+        sub.setModal(true);
+        sub.setResizable(false);
+        sub.setClosable(false);
+        sub.setWidth(300.0f, Unit.PIXELS);
+        sub.setHeight(150.0f, Unit.PIXELS);
+        sub.center();
+        VerticalLayout content = new VerticalLayout();
+        content.addComponent(new Label("Are you sure, you want to remove?"));
+        content.addComponent(new Button("Ok", e -> {
+            contactUI.getContactService().delete(contact);
+            contactUI.updateContacts();
+            setVisible(false);
+            sub.close();
+            Notification.show("Contact Removed");
+        }));
+        sub.setContent(content);
+
+        UI.getCurrent().addWindow(sub);
+    }
+}
---
+++ src/main/java/com/example/vaadinexample/ui/contact/ContactUI.java
@@ -0,0 +1,67 @@
+package com.example.vaadinexample.ui.contact;
+
+import com.example.vaadinexample.model.Contact;
+import com.example.vaadinexample.service.ContactService;
+import com.example.vaadinexample.ui.menu.MenuFormFactory;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.*;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringUI(path = "/contact")
+@Title("Contacts")
+@Theme("valo")
+public class ContactUI extends UI {
+
+    @Autowired
+    @Getter
+    private ContactService contactService;
+
+    @Autowired
+    private MenuFormFactory menuFormFactory;
+
+    private Button add = new Button("Add");
+    private Grid<Contact> grid = new Grid<>(Contact.class);
+    private ContactForm contactForm = new ContactForm(this);
+
+    private Panel logoPanel = new Panel();
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+        VerticalLayout main = new VerticalLayout();
+        Component menu = menuFormFactory.createComponent();
+        HorizontalLayout layout1 = new HorizontalLayout();
+
+        grid.setColumns("firstName", "lastName");
+        layout1.addComponents(grid,contactForm);
+
+        logoPanel.setWidth("5%");
+        logoPanel.setHeight("5%");
+        logoPanel.setContent(new Label("Lo"));
+
+        updateContacts();
+        main.addComponents(/*logoPanel,*/menu,add, layout1);
+        setContent(main);
+
+        contactForm.setVisible(false);
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            if(e.getValue() == null) {
+                contactForm.setVisible(false);
+            } else {
+                contactForm.setContact(e.getValue());
+            }
+        });
+        add.addClickListener(e -> {
+            grid.asSingleSelect().clear();
+            contactForm.setContact(new Contact());
+        });
+    }
+
+    public void updateContacts() {
+        grid.setItems(contactService.findAll());
+    }
+}
